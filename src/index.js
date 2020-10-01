88438ede0c9e18004dbab824a554e25da411ee3d
Vagiz Musin <vagiz_mus@mail.ru>
2020-10-01 21:56:38 +0600

    feat: add reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let c = Math.abs(n) + "";
+    let reverse = "";
+    for (let i = c.length - 1; i >= 0; i--) {
+        reverse = reverse + c[i];
+    }
+    return Number(reverse);
 }
